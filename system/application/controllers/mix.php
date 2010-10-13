837bf28d5340e966f8b3fa1f9c033156678de4a2
Ian Thomas <guapo42@gmail.com>
2010-10-12 23:11:30 -0400

    Initial commit of base setup

---
+++ system/application/controllers/mix.php
@@ -0,0 +1,17 @@
+<?php
+
+class Mix extends Controller {
+
+	function Mix()
+	{
+		parent::Controller();	
+	}
+	
+	function index()
+	{
+		$this->load->view('template');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./system/application/controllers/welcome.php */
